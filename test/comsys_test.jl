196c11f07dda61adf893487e228df15f68263d9e
Alexander A Bouman <alex.bouman@gmail.com>
2021-11-08 16:59:41 -0500

    Added tests to check mantains Float32

--- test/comsys_test.jl
+++ test/comsys_test.jl
@@ -1,9 +1,11 @@
 using Revise
 import EKF
 import EKF.CommonSystems as ComSys
+using LinearAlgebra
 using StaticArrays
 using Test
 using BenchmarkTools
+
 
 @testset "Common Systems Performance Evaluations" begin
 
@@ -17,6 +19,8 @@ using BenchmarkTools
         meas::M,
         measErr::EKF.ErrorMeasurement
     ) where {M <: EKF.Measurement}
+        @assert eltype(state) == eltype(stateErr) == eltype(input) == eltype(meas) == eltype(measErr)
+
         @testset "$name" begin
             dt = 0.1
 
@@ -43,6 +47,22 @@ using BenchmarkTools
             b = @benchmark EKF.error_measure_jacobian($M, $state)
             @test maximum(b.gctimes) == 0  # no garbage collection
             @test b.memory == 0            # no dynamic memory allocations
+
+            # Check that filtering on a vector of type T produces a vector of type T
+            T = eltype(state)
+            dt = T(0.1)
+            est_cov = Matrix{T}(.3 * I(length(stateErr)))
+            process_cov = Matrix{T}(.3 * I(length(stateErr)))
+            ekf = EKF.ErrorStateFilter{typeof(state), typeof(stateErr), typeof(input)}(state, est_cov, process_cov)
+            oriObs = EKF.Observation(
+                meas,
+                SMatrix{length(measErr),length(measErr),T}(I(length(measErr))),
+            )
+            EKF.prediction!(ekf, input, dt)
+            EKF.update!(ekf, oriObs)
+
+            @test eltype(ekf.est_state) == eltype(state)
+            @test eltype(ekf.est_cov) == eltype(state)
         end
     end
 
@@ -55,6 +75,22 @@ using BenchmarkTools
 
     performance_test("IMU/Vicon Filter: Float32", state, stateErr, input, meas, measErr)
 
+    # dt = Float32(0.1)
+    # est_cov = Matrix{Float32}(.3 * I(length(stateErr)))
+    # process_cov = Matrix{Float32}(.3 * I(length(stateErr)))
+    # ekf = EKF.ErrorStateFilter{ComSys.ImuState, ComSys.ImuError, ComSys.ImuInput}(state, est_cov, process_cov)
+    # oriObs = EKF.Observation(
+    #     meas,
+    #     SMatrix{6,6,Float32}([i==j ? 1. : 0. for i = 1:6, j = 1:6]),
+    # )
+
+    # EKF.prediction!(ekf, input, dt)
+    # EKF.update!(ekf, oriObs)
+    # display(ekf.est_state)
+    # display(ekf.est_cov)
+    # ekf = EKF.ErrorStateFilter{typeof(state), typeof(stateErr), typeof(input)}(state, est_cov, process_cov)
+
+
     state = ComSys.ImuState{Float64}(zeros(3)..., [1.,0,0,0]..., zeros(9)...)
     stateErr = ComSys.ImuError{Float64}(zeros(15)...)
     input = ComSys.ImuInput{Float64}(zeros(6))
@@ -62,5 +98,4 @@ using BenchmarkTools
     measErr = ComSys.ViconError{Float64}(zeros(6))
 
     performance_test("IMU/Vicon Filter: Float64", state, stateErr, input, meas, measErr)
-
 end
